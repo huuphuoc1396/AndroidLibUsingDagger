36b06f1f9b44d5106c80bfc0e7e344a008767e96
huuphuoc1396 <huuphuoc1396@gmail.com>
2022-01-21 10:40:32 +0700

    Demo integrates library

---
+++ lib-demo/src/main/java/com/example/lib_demo/LibClient.kt
@@ -0,0 +1,11 @@
+package com.example.lib_demo
+
+import android.content.Context
+import com.example.lib.LibActivity
+
+object LibClient {
+
+    fun start(context: Context) {
+        LibActivity.start(context)
+    }
+}
